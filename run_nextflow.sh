a01ca9b2e92d392694433b7b5c9230918600b5cf
“lonnekevanbrussel” <lonneke.vanbrussel@uvic.cat>
2024-01-05 11:45:45 +0100

    changed links to NextflowModules

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-workflow_path='/hpc/diaggen/users/lonneke/github/DxNextflowRNA'
+workflow_path='/hpc/diaggen/users/lonneke/github/DxNextflowRNA/NextflowModules'
 
 # Set input and output dirs
 input=`realpath $1`
@@ -31,8 +31,8 @@ sbatch <<EOT
 #SBATCH --mail-type FAIL
 #SBATCH --account=diaggen
 
-/hpc/diaggen/software/development/DxNextflowRNA/tools/nextflow run /hpc/diaggen/users/lonneke/github/DxNextflowRNA/main.nf  \
--c /hpc/diaggen/users/lonneke/github/DxNextflowRNA/nextflow.config -resume -ansi-log false -profile slurm \
+/hpc/diaggen/software/development/DxNextflowRNA/tools/nextflow run /hpc/diaggen/users/lonneke/github/DxNextflowRNA/NextflowModules/main.nf  \
+-c /hpc/diaggen/users/lonneke/github/DxNextflowRNA/NextflowModules/nextflow.config -resume -ansi-log false -profile slurm \
 --input $input \
 --outdir $output \
 --email $email
