cefbef6bf4f7dc1f3ab50d812a98f6c449c4083c
lonnekevanbrussel <125465018+lonnekevanbrussel@users.noreply.github.com>
2024-02-20 13:19:41 +0100

    Added filtering, findEncodingDim, controlForConfounders, sampleExclusionMask, fitting, pvalue and zscore calculation to run outrider

--- Outrider/1.20.0/outrider.R
+++ Outrider/1.20.0/outrider.R
@@ -31,9 +31,10 @@ read_input_files <- function(input){
       return(as_tibble(rownames_to_column(as.data.frame(assays(rse)$counts), var="rownames")))
     } else if (input_ext %in% c("txt", "tsv")) {
       count_table <- read_delim(f, show_col_types=FALSE, skip=1)
+      # col 1: samples IDs, col 7: counts
       ct <- count_table[,c(1,7)]
       names(ct)[1] <- "rownames"
-      sampleIDs <- append(sampleIDs, colnames(count_table))
+      sampleIDs <<- append(sampleIDs, colnames(ct)[2])
       return(ct)
     } else {
       stop("Input file extension is not supported.")
@@ -75,17 +76,64 @@ run_outrider <- function(all_counts) {
   rownames(all_counts_matrix) <- all_counts$rownames
 
   ods <- OutriderDataSet(countData=all_counts_matrix)
-  filtered <- filterExpression(ods, minCounts = TRUE, filterGenes = TRUE)
-  out <- OUTRIDER(filtered)
+  filtered <- filterExpression(ods, minCounts = FALSE, filterGenes = FALSE, gtfFile="/hpc/diaggen/data/databases/gencode/gencode.v44.primary_assembly.basic.annotation.gtf")
+  filtered <- filtered[mcols(filtered)$passedFilter,]
+#  filtered <- filterExpression(ods, minCounts = TRUE, filterGenes = TRUE)
+
+  ods <- estimateSizeFactors(filtered)
+  print(ods$sizeFactor)
+
+  ##Drop pipeline
+  #maxTestedDimensionProportion 
+  mp <- 3	 
+  implementation <- "autoencoder"
+
+  a <- 5 
+  b <- min(ncol(ods), nrow(ods)) / mp   # N/3
+  maxSteps <- 15
+   if(mp < 4){
+    maxSteps <- 20
+  }
+
+  Nsteps <- min(maxSteps, b)   # Do at most 20 steps or N/3
+  # Do unique in case 2 were repeated
+
+  pars_q <- round(exp(seq(log(a),log(b),length.out = Nsteps))) %>% unique
+  #help suggestion:
+  #pars_q <- seq(2, min(100, ncol(ods) - 1, nrow(ods) - 1), 2)
+  print(pars_q)
+
+  ods <- findEncodingDim(ods, params = pars_q, implementation = implementation, BPPARAM=MulticoreParam(8))
+
+  print("getbestQ")
+  opt_q <- getBestQ(ods)
+  print(opt_q)
+
+  ods <- controlForConfounders(ods, q=opt_q, implementation=implementation, BPPARAM=MulticoreParam(8))
+  
+  #exclude sample of interest from the fitting bc of possible replicates..
+  #message(date(), ": sample Exclusion Mask ...")
+  #sampleExclusionMask(ods) <- FALSE
+  #sampleExclusionMask(ods[,c(1)]) <- TRUE
+
+  #if(grepl("^(peer|pca)$", implementation)){ 
+  #   message(date(), ": Fitting the data ...")
+  #   ods <- fit(ods, BPPARAM=MulticoreParam(8))
+  #}
+
+  ods <- computePvalues(ods, alternative="two.sided", method="BY")
+  out <- computeZscores(ods)
+
+#  out <- OUTRIDER(filtered, BPPARAM=MulticoreParam(8))
   return(out)
 }
 
 
-save_output <- function(out_path, out_outrider, ref_samples, feature, padj_thres=0.05, zscore_thres=0, all=True) {
-#  res <- as_tibble(results(ods, padjCutoff=padj_thres, zScoreCutoff=zscore_thres, all=TRUE))
-  res <- as_tibble(results(out_outrider, padjCutoff=padj_thres, zScoreCutoff=zscore_thres, all=TRUE))
+save_output <- function(out_path, out_outrider, ref_samples, feature, padj_thres=0.05, zscore_thres=0, a=TRUE) {
+  res <- as_tibble(results(out_outrider, padjCutoff=padj_thres, zScoreCutoff=zscore_thres, all=a))
   # Reference samples are excluded from final results. 
   query_res <- filter(res, !(sampleID %in% ref_samples))
+  #query_res <- query_res[order(query_res$pValue),]
   # Write output table with aberrant expressed targets.
   write_tsv(query_res, paste0(out_path, "outrider_result_", feature, ".tsv"))
 }
