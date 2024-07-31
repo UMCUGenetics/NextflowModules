0c330def3f83835a9a08f39e46a0a8b952514844
lonnekevanbrussel <125465018+lonnekevanbrussel@users.noreply.github.com>
2024-07-31 17:21:41 +0200

    Fraser edits for readability and logging

--- Fraser/1.99.3/fraser.R
+++ Fraser/1.99.3/fraser.R
@@ -1,4 +1,3 @@
-
 # load FRASER library
 suppressPackageStartupMessages(library(FRASER))
 suppressPackageStartupMessages(library(biomaRt))
@@ -12,7 +11,7 @@ get_st_rows <- function(input){
     sampleID <- list.files(path=input,pattern="*.bam",full.names=FALSE)
     bamFile <- list.files(path=input,pattern="*.bam",full.names=TRUE)
     return(data.table(sampleID,bamFile))
-  }
+  }  
   else if(file.exists(input)){
     sampleID <- basename(file.path(input))
     bamFile <- file.path(input)
@@ -46,7 +45,8 @@ run_fraser <- function(sampleTable){
   fds <- countRNAData(settings)
 
   fds <- calculatePSIValues(fds)
-    #FRASER vignette/gagneurlab settings:
+
+  #FRASER vignette/gagneurlab settings:
   #Currently, we suggest keeping only junctions which support the following:
   #minExpressionInOneSample - 20 - At least one sample has 20 (or more) reads
   #quantileMinExpression - 10 - The minimum total read count (N) an intron needs to have at the specified quantile across samples to pass the filter. See quantileForFiltering. 
@@ -56,19 +56,22 @@ run_fraser <- function(sampleTable){
 
   fds <- filterExpressionAndVariability(fds, minExpressionInOneSample = 20, quantileMinExpression=10, quantile = 0.95, minDeltaPsi=0.05, filter=FALSE)
   fds <- saveFraserDataSet(fds, name="fds-filter")
-#  plotheat = "plots_fraser.pdf"
-#  pdf(plotheat,onefile = TRUE)
-#  plotFilterExpression(fds, bins=100)
+  plotheat = "plots_fraser.pdf"
+  pdf(plotheat,onefile = TRUE)
+  plotFilterExpression(fds, bins=100)
 
   message(date(), ": dimension before filtering...")
-  print(dim(fds))
+  print(dim(assays(fds)$rawCountsJ))
+  print(dim(assays(fds)$rawCountsSS))
+
   fds <- fds[mcols(fds, type="j")[,"passed"]]
   message(date(), ": dimension after filtering...")
-  print(dim(fds))
+  print(dim(assays(fds)$rawCountsJ)) 
+  print(dim(assays(fds)$rawCountsSS))
 
   #Finds the optimal encoding dimension by injecting artificial splicing outlier ratios while maximizing the precision-recall curve.
   # Get range for latent space dimension, max tested dimensions = 6
-  mp <- 6
+  mp <- 6 
   a <- 2
   b <- min(ncol(fds), nrow(fds)) / mp   # N/mp
 
@@ -101,7 +104,7 @@ run_fraser <- function(sampleTable){
   message(date(), "...find best q: " ,bestq)
 
   # Add verbosity to the FRASER object
-  verbose(fds) <- 3
+  verbose(fds) <- 3 
   fds <- fit(fds, q=bestq, type=psiType, implementation=impl, iterations=15)
 
   # Heatmap of the sample correlation
@@ -113,18 +116,20 @@ run_fraser <- function(sampleTable){
 
   #all in one getEncDimRange, optimHyperParams, bestQ, fit
   #fds_fraser <- FRASER(fds_filtered, q=c(jaccard=2))
-
+  
   fds <- annotateRanges(fds, GRCh = 38)
 
   # Pvalues
   fds <- calculatePvalues(fds, type=psiType)
   # Adjust Pvalues
   fds <- calculatePadjValues(fds, type=psiType)
+  
 
   fds <- saveFraserDataSet(fds, name="fds-final")
-
+  
   return (fds)
 }
+
 
 write_fraser_output <- function(fraser_ds, prefix){
   # retrieve results with default and recommended cutoffs (padj <= 0.05 (0.1 gagneurlab) and # |deltaPsi| >= 0.3)
